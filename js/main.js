74f4f987449529e77ba3e9a034b8f0092390ab61
RichardAybar <theenvykillz@gmail.com>
2018-10-15 07:58:19 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -34,15 +34,27 @@ var checkForMatch = function () {
   }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = parseInt(this.getAttribute('data-id'),10);
   console.log("User flipped" + cards[cardId].rank);
+  this.src = cards[cardId].cardImage; 
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 
-}
+};
 
-  flipCard(0);
-  flipCard(2);
 
+ var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+createBoard(); 
